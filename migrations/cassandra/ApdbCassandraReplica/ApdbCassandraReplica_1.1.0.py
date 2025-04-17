f6fa910238cc2c6b6a33141d04c1b5b6ee81562c
Andy Salnikov <salnikov@slac.stanford.edu>
2025-04-17 12:02:17 -0700

    Add table_schema module for parsing/generating table schema.
    This simplifies ApdbCassandraReplica_1.1.0.py scrip and can be useful
    for future migrations.

--- migrations/cassandra/ApdbCassandraReplica/ApdbCassandraReplica_1.1.0.py
+++ migrations/cassandra/ApdbCassandraReplica/ApdbCassandraReplica_1.1.0.py
@@ -6,9 +6,9 @@ Create Date: 2025-04-14 16:19:00.653340
 """
 
 import logging
-from typing import Any, NamedTuple
 
 from lsst.dax.apdb_migrate.cassandra.context import Context, _Context
+from lsst.dax.apdb_migrate.cassandra.table_schema import Column, TableSchema
 
 # revision identifiers, used by Alembic.
 revision = "ApdbCassandraReplica_1.1.0"
@@ -17,14 +17,6 @@ branch_labels = None
 depends_on = None
 
 _LOG = logging.getLogger(__name__)
-
-
-class _Column(NamedTuple):
-    column_name: str
-    type: str
-    kind: str
-    position: int
-    clustering_order: str | None
 
 
 def upgrade() -> None:
@@ -40,6 +32,11 @@ def upgrade() -> None:
       - DiaSourceToPartition table adds int column `apdb_replica_subchunk`.
     """
     with Context(revision) as ctx:
+        # Make new tables first, this has higher chance of failure.
+        _make_new_chunks_table(ctx, "DiaObjectChunks")
+        _make_new_chunks_table(ctx, "DiaSourceChunks")
+        _make_new_chunks_table(ctx, "DiaForcedSourceChunks")
+
         table = "ApdbReplicaChunks"
         column = "has_subchunks"
         _LOG.info("Adding column %s to table %s", column, table)
@@ -52,9 +49,7 @@ def upgrade() -> None:
         query = f'ALTER TABLE "{ctx.keyspace}"."{table}" ADD "{column}" INT'
         ctx.update(query)
 
-        _make_new_chunks_table(ctx, "DiaObjectChunks")
-        _make_new_chunks_table(ctx, "DiaSourceChunks")
-        _make_new_chunks_table(ctx, "DiaForcedSourceChunks")
+        _update_frozen_config(ctx)
 
 
 def downgrade() -> None:
@@ -71,112 +66,29 @@ def _make_new_chunks_table(ctx: _Context, table_name: str) -> None:
     partitiong key. The rest of the columns and table options are copied from
     the existing table.
     """
-    # Get the list of columns from existing table.
-    query = (
-        "select column_name, type, kind, position, clustering_order "
-        "from system_schema.columns "
-        "where keyspace_name = %s and table_name=%s"
-    )
-    columns: list[_Column] = list(ctx.query(query, (ctx.keyspace, table_name)))
+    table_schema = TableSchema.from_table(ctx, table_name)
+    if len(table_schema.partitioning_columns) != 1:
+        raise TypeError(f"Unexpected number of columns in partition key: {table_schema.partitioning_columns}")
 
-    # Make a list of new partitioning columns, we expect that old table must
-    # have exactly one partitioning column (apdb_replica_chunk).
-    part_columns = [column.column_name for column in columns if column.kind == "partition_key"]
-    if len(part_columns) != 1:
-        raise TypeError(f"Unexpected number of columns in partition key: {part_columns}")
-    new_column = _Column(
+    # New column to add to partitioning key.
+    new_column = Column(
         column_name="apdb_replica_subchunk",
         type="int",
         kind="partition_key",
-        position=99,
-        clustering_order=None,
+        position=1,
     )
-    part_columns.append(new_column.column_name)
-    columns.append(new_column)
+    table_schema.columns.append(new_column)
 
-    # Make an ordered list of clustering columns.
-    clustering_columns = [column for column in columns if column.kind == "clustering"]
-    clustering_columns.sort(key=lambda col: col.position)
+    table_schema.table_name = f"{table_schema.table_name}2"
+    table_ddl = table_schema.make_ddl()
 
-    # We do not expect DESC order on clustering column. Potentially we could
-    # add CLUSTERING_ORDER option but it's unlikely we need it.
-    if any(column.clustering_order != "asc" for column in clustering_columns):
-        raise TypeError("Non-ascending clustering order is not supported")
+    _LOG.info("Creating table %s", table_schema.table_name)
+    ctx.update(table_ddl)
 
-    # Make new primary key.
-    part_columns = [f'"{name}"' for name in part_columns]
-    part_key = f"({', '.join(part_columns)})"
-    pk_list = [part_key] + [f'"{column.column_name}"' for column in clustering_columns]
-    pk = f"PRIMARY KEY ({', '.join(pk_list)})"
 
-    def _column_sort_key(column: _Column) -> tuple:
-        """Define colum ordering , partitoning columns first, then clustering,
-        then all the rest.
-        """
-        key = []
-        if column.kind == "partition_key":
-            key.append(0)
-            key.append(column.position)
-        elif column.kind == "clustering":
-            key.append(1)
-            key.append(column.position)
-        else:
-            key.append(2)
-            key.append(column.column_name)
-        return tuple(key)
-
-    # Sort all columns.
-    columns.sort(key=_column_sort_key)
-
-    # Generate table DDL.
-    table_column_defs = [f'"{column.column_name}" {column.type}' for column in columns]
-    table_column_defs.append(pk)
-    column_defs = ",\n    ".join(table_column_defs)
-
-    new_table_name = f"{table_name}2"
-    table_def = f'create table "{ctx.keyspace}"."{new_table_name}" (\n    {column_defs}\n)'
-
-    # Also copy applicable table options.
-    query = "select * from system_schema.tables where keyspace_name = %s and table_name = %s"
-    rows = list(ctx.query(query, (ctx.keyspace, table_name)))
-    options_tuple = rows[0]
-
-    def _quote(value: Any) -> str:
-        """Render values in options declaration."""
-        if isinstance(value, (str, int, float)):
-            return repr(value)
-        return str(value)
-
-    # Names of options we want to copy.
-    option_names = (
-        "additional_write_policy",
-        "allow_auto_snapshot",
-        "bloom_filter_fp_chance",
-        "caching",
-        "cdc",
-        "comment",
-        "compaction",
-        "compression",
-        "crc_check_chance",
-        "default_time_to_live",
-        "extensions",
-        "gc_grace_seconds",
-        "incremental_backups",
-        "max_index_interval",
-        "memtable_flush_period_in_ms",
-        "min_index_interval",
-        "read_repair",
-        "speculative_retry",
-    )
-
-    options = []
-    for option_name in option_names:
-        option_value = getattr(options_tuple, option_name, None)
-        if option_value is not None:
-            options.append(f"{option_name} = {_quote(option_value)}")
-
-    options_str = "\n    AND ".join(options)
-    table_def = f"{table_def} with {options_str}"
-
-    _LOG.info("Creating table %s", new_table_name)
-    ctx.update(table_def)
+def _update_frozen_config(ctx: _Context) -> None:
+    """Update configuration stored in metadata."""
+    config = ctx.get_apdb_config()
+    # Use default value for this parameter.
+    config["replica_sub_chunk_count"] = 64
+    ctx.store_apdb_config(config)
